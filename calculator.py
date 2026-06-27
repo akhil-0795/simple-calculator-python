c72959bb3cab2127c042507f9f09ae4fe241343c
Peddi Akhil <peddiakhil0795@gmail.com>
2026-06-27 20:23:32 +0530

    Add files via upload
    # 🧮 Python Simple Calculator
    
    ## 📌 Description
    
    This is my first Python project created while learning Python.
    
    The calculator takes two numbers as input from the user and performs:
    
    - ➕ Addition
    - ➖ Subtraction
    - ✖️ Multiplication
    - ➗ Division
    - 🟡 Remainder
    - 🔢 Power
    - 📊 Floor Division
    
    ## 🛠️ Technologies Used
    
    - Python 3
    
    ## 📚 Concepts Learned
    
    - Variables
    - Input and Output
    - Data Types
    - Arithmetic Operators
    
    ## ▶️ How to Run
    
    ```bash
    python calculator.py
    ```
    
    ## 👨‍💻 Author
    
    **Akhil**
    
    B.Tech CSE Student | Python Learner 🚀
    
    ⭐ This is my first project in my Python learning journey.

---
+++ calculator.py
@@ -0,0 +1,17 @@
+
+print("=====simple calculator======")
+
+
+print("\nResults")
+print("------------------------")
+
+
+num1=int(input("enter your first  number"))
+num2=int(input("enter your second number"))
+print("Addition=",num1+num2)
+print("Subtraction=",num1-num2)
+print("Division=",num1/num2)
+print("Multiplication=",num1*num2)
+print("Reminder=",num1%num2)
+print("Power=",num1**num2)
+print("Floor division=",num1//num2)
